9132bac5bc9a664b590791172770e1f0f90d7bc6
PhilopateerWagdy <philowagdy20@gmail.com>
2022-05-26 21:15:02 +0200

    Link ML Model to the Backend to detect news

--- roots/newsRoutes.js
+++ roots/newsRoutes.js
@@ -7,11 +7,11 @@ const newsController = require('./../controllers/newsController');
 const router = express.Router();
 
 router.route('/recentlySearchedNews').get(newsController.recentlySearchedNews);
-router.route('/guestDetectNews').post(newsController.guestDetectNews);
+router.route('/guestDetectNews').post(newsController.callFlaskAPI, newsController.guestDetectNews);
 
 router.use( authController.protect, authController.restrict("user") );
 router.route('/userHistory').get(newsController.deleteMonthAgoNews, newsController.userHistory);
-router.route('/userDetectNews').post(newsController.userDetectNews);
+router.route('/userDetectNews').post(newsController.callFlaskAPI, newsController.userDetectNews);
 router.route('/:id').delete(newsController.deleteNews);
 
 module.exports = router;
